65423c9b6fc8ee292aad4102a0e10c9fe955d171
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-01 12:06:09 +0100

    Added React Router Dom

--- za-movie-jokes/src/app/App.jsx
+++ za-movie-jokes/src/app/App.jsx
@@ -1,11 +1,12 @@
-
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import MoviesList from "../features/MoviesList";
 
 export default function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <Routes>
+      <Route path="/">
+        <Route index element={MoviesList} />
+      </Route>
+    </Routes>
+  );
 }
