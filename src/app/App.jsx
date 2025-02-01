a51e174ad97fb402ec9bfd89156c7f4783f6b636
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-01 12:39:22 +0100

    Improved Imports & Folder Structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import MoviesList from "../features/MoviesList";
+import MoviesList from "src/features/MoviesList";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/">
-        <Route index element={MoviesList} />
+        <Route index element={<MoviesList />} />
       </Route>
     </Routes>
   );
