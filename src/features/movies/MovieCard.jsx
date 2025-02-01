31c4e685ba065a3ecfc04f7710b182b99c09740b
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-01 19:20:41 +0100

    Implemented Card Styles

--- src/features/movies/MovieCard.jsx
+++ src/features/movies/MovieCard.jsx
@@ -1,35 +1,36 @@
 import {
   Button,
-  ButtonGroup,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
-  Divider,
   Heading,
   Image,
-  Stack,
   Text,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function MovieCard({ id, title, poster, overview }) {
   return (
-    <Card maxW="sm">
-      <CardHeader>
+    <Card borderRadius={30} bg="rgb(255,255,255);" shadow="2xl">
+      <CardHeader pb={0}>
         <Image
+          m="auto"
           src={poster}
-          alt="Green double couch with wooden legs"
+          alt={`${title}'s poster`}
           borderRadius="lg"
+          mb={2}
         />
       </CardHeader>
-      <CardBody>
-        <Heading>{title}</Heading>
-        <Text>{overview}</Text>
+      <CardBody minH={130}>
+        <Heading size="sm" pb={2}>
+          {title}
+        </Heading>
+        <Text color="gray.700">{overview.substring(0, 50) + "..."}</Text>
       </CardBody>
-      <CardFooter>
-        <Link to={`/movies/${id}`}>
-          <Button variant="solid" colorScheme="blue">
+      <CardFooter pt={2}>
+        <Link to={`/movies/${id}`} style={{ width: "100%" }}>
+          <Button m="auto" bg="green.300" color="white" w="100%">
             View joke & more
           </Button>
         </Link>
--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -7,20 +7,18 @@ import { MOVIEDB_IMAGES_URL } from "src/common/constants";
 function MoviesList() {
   return (
     <>
-      <Heading textAlign="center" size="xl">
+      <Heading textAlign="center" size="xl" mb={4}>
         Trending Movies
       </Heading>
       <SimpleGrid spacing={4} columns={{ sm: 1, md: 3, lg: 4 }}>
         {data.results.map((movie) => (
-          <Link key={movie.id} to={`/movies/${movie.id}`}>
-            {movie.title}
-            <MovieCard
-              id={movie.id}
-              title={movie.title}
-              poster={`${MOVIEDB_IMAGES_URL}/${movie.poster_path}`}
-              overview={movie.overview}
-            />
-          </Link>
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster={`${MOVIEDB_IMAGES_URL}/${movie.poster_path}`}
+            overview={movie.overview}
+          />
         ))}
       </SimpleGrid>
     </>
