6ece2eab74f72d952386f78992cbd65d21f082c2
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-01 16:39:31 +0100

    Added Chakra UI

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import MovieDetails from "src/features/movies/MovieDetails";
 import MoviesList from "src/features/movies/MoviesList";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<MoviesList />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-      </Route>
-    </Routes>
+    <ChakraProvider>
+      <Routes>
+        <Route path="/">
+          <Route index element={<MoviesList />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </ChakraProvider>
   );
 }
