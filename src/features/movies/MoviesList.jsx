61985d42e1a121a805be1a2280b0cf9071b4a047
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-01 18:10:46 +0100

    Added Cards with Grid Layout

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import { data } from "./data";
+import MovieCard from "./MovieCard";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 
 function MoviesList() {
   return (
-    <div>
-      {data.results.map((movie) => (
-        <Link key={movie.id} to={`/movies/${movie.id}`}>
-          {movie.title}
-        </Link>
-      ))}
-    </div>
+    <>
+      <Heading textAlign="center" size="xl">
+        Trending Movies
+      </Heading>
+      <SimpleGrid spacing={4} columns={{ sm: 1, md: 3, lg: 4 }}>
+        {data.results.map((movie) => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            {movie.title}
+            <MovieCard />
+          </Link>
+        ))}
+      </SimpleGrid>
+    </>
   );
 }
 
