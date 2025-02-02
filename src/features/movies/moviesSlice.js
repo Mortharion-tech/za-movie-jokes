f16b53c2ff8c24d4c1d3950e9d57b77cdcf7299a
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-02 21:08:03 +0100

    Implemented Slice and Store

---
+++ src/features/movies/moviesSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+  name: "movies",
+  initialState: data,
+  reducers: {},
+});
+
+export default moviesSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app/App.jsx";
+import { store } from "src/app/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
