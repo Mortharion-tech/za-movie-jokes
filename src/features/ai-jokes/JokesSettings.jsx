0f25528c6a1556c4693c452075b47c01ad2bae37
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-02 23:38:27 +0100

    Implemented Settings with Form

--- src/features/ai-jokes/JokesSettings.jsx
+++ src/features/ai-jokes/JokesSettings.jsx
@@ -1,6 +1,13 @@
-import { SettingsIcon } from "@chakra-ui/icons";
+import { DeleteIcon, SettingsIcon } from "@chakra-ui/icons";
 import {
+  Badge,
   Button,
+  Flex,
+  FormControl,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftAddon,
   Popover,
   PopoverArrow,
   PopoverBody,
@@ -10,9 +17,20 @@ import {
   PopoverHeader,
   PopoverTrigger,
   Portal,
+  Text,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 function JokesSettings() {
+  const [rules, setRules] = useState([
+    { name: "Joke type", description: "Programmer" },
+  ]);
+
+  const handleRuleRemove = (ruleName) => {};
+  const handleAddRule = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Popover placement="bottom-start">
       <PopoverTrigger>
@@ -23,12 +41,53 @@ function JokesSettings() {
       <Portal>
         <PopoverContent>
           <PopoverArrow />
-          <PopoverHeader>Header</PopoverHeader>
+          <PopoverHeader>Joke Rules</PopoverHeader>
           <PopoverCloseButton />
           <PopoverBody>
-            <Button colorScheme="blue">Button</Button>
+            <Flex width="100%" direction="column">
+              {rules.map((rule) => (
+                <Flex
+                  key={rule.name}
+                  width="100%"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  mb={1}
+                >
+                  <Text>
+                    <Badge mr={2} colorScheme="purple">
+                      {rule.name}
+                    </Badge>
+                    <small>{rule.description}</small>
+                  </Text>
+                  <IconButton
+                    onClick={() => handleRuleRemove(rule.name)}
+                    variant="outline"
+                    icon={<DeleteIcon />}
+                    size="sm"
+                  />
+                </Flex>
+              ))}
+            </Flex>
           </PopoverBody>
-          <PopoverFooter>This is the footer</PopoverFooter>
+          <PopoverFooter>
+            <form onSubmit={handleAddRule}>
+              <FormControl isRequired>
+                <InputGroup size="sm" mb={2}>
+                  <InputLeftAddon>Name:</InputLeftAddon>
+                  <Input type="text" />
+                </InputGroup>
+              </FormControl>{" "}
+              <FormControl isRequired>
+                <InputGroup size="sm" mb={2}>
+                  <InputLeftAddon>Description:</InputLeftAddon>
+                  <Input type="text" />
+                </InputGroup>
+              </FormControl>
+              <Button size="sm" type="submit" alignSelf="flex-end">
+                Submit
+              </Button>
+            </form>
+          </PopoverFooter>
         </PopoverContent>
       </Portal>
     </Popover>
