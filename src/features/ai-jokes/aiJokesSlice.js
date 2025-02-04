e5dfd8e2def0cfd70a6ce60cfe5e5e498634b8b4
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-04 17:34:14 +0100

    Added AI Jokes Thunk & Reducers

--- src/features/ai-jokes/aiJokesSlice.js
+++ src/features/ai-jokes/aiJokesSlice.js
@@ -1,11 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  rules: [{ name: "Joke type", description: "Programmer" }],
+  jokes: {
+    jokes: [],
+    status: "idle",
+    error: null,
+  },
+};
+
+export const fetchJoke = createAsyncThunk(
+  "aiJokes/fetchJoke",
+  async ({ movieId }) => {
+    return { movieId, joke: "Funny joke!" };
+  }
+);
 
 const aiJokesSlice = createSlice({
   name: "ai-jokes",
-  initialState: {
-    rules: [{ name: "Joke type", description: "Programmer" }],
-    jokes: [],
-  },
+  initialState,
   reducers: {
     ruleAdded(state, action) {
       const ruleIndex = state.rules.findIndex(
@@ -21,6 +34,28 @@ const aiJokesSlice = createSlice({
       if (ruleIndex < 0) return;
       state.rules.splice(ruleIndex, 1);
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchJoke.pending, (state) => {
+        state.jokes.status = "loading";
+      })
+      .addCase(fetchJoke.fulfilled, (state, action) => {
+        state.jokes.status = "succeeded";
+        const jokeIndex = state.jokes.jokes.findIndex(
+          (joke) => joke.movieId === action.payload.movieId
+        );
+
+        if (jokeIndex > -1) {
+          state.jokes.jokes[jokeIndex] = action.payload;
+        } else {
+          state.jokes.jokes.push(action.payload);
+        }
+      })
+      .addCase(fetchJoke.rejected, (state, action) => {
+        state.jokes.status = "failed";
+        state.jokes.error = action.error.message;
+      });
   },
 });
 
