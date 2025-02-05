a932c0c56126660779a8cddba5008c3478f7627f
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-05 21:40:17 +0100

    Update moviesSlice.js

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -1,24 +1,15 @@
 import {
   createAsyncThunk,
-  createEntityAdapter,
+  createSelector,
   createSlice,
 } from "@reduxjs/toolkit";
 import { fetchPopularMovies } from "./api";
 
-/* const initialState = {
+const initialState = {
   movies: [],
   status: "idle", // 'idle' / 'loading' / 'succeeded' / 'failed'
   error: null,
-}; */
-
-const moviesAdapter = createEntityAdapter({
-  sortComparer: (movieA, movieB) => movieB.vote_average - movieA.vote_average,
-});
-
-const initialState = moviesAdapter.getInitialState({
-  status: "idle", // 'idle' / 'loading' / 'succeeded' / 'failed'
-  error: null,
-});
+};
 
 export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
   const data = await fetchPopularMovies();
@@ -36,8 +27,7 @@ const moviesSlice = createSlice({
       })
       .addCase(fetchMovies.fulfilled, (state, action) => {
         state.status = "succeeded";
-        /* state.movies = action.payload.results; */
-        moviesAdapter.setMany(state, action.payload.results);
+        state.movies = action.payload.results;
       })
       .addCase(fetchMovies.rejected, (state, action) => {
         state.status = "failed";
@@ -46,19 +36,17 @@ const moviesSlice = createSlice({
   },
 });
 
-/* export const selectAllMovies = createSelector(
+export const selectAllMovies = createSelector(
   [(state) => state.movies.movies],
   (mov) => {
     const movies = [...mov];
     movies.sort((movieA, movieB) => movieB.vote_average - movieA.vote_average);
     return movies;
   }
-); */
-
-export const { selectAll: selectAllMovies, selectById: selectMovieById } =
-  moviesAdapter.getSelectors();
-
+);
 export const selectMoviesStatus = (state) => state.movies.status;
 export const selectMoviesError = (state) => state.movies.error;
+export const selectMovieById = (state, movieId) =>
+  state.movies.movies.find((movie) => movie.id === Number(movieId));
 
 export default moviesSlice.reducer;
