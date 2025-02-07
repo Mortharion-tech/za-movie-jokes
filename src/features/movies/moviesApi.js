6d6bcb0dd15321b0ea00540526bcb2b3c4bbdde2
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-07 05:47:19 +0100

    Code Cleanup

--- src/features/movies/moviesApi.js
+++ src/features/movies/moviesApi.js
@@ -1,10 +1,3 @@
-/* import {
-  createAsyncThunk,
-  createSelector,
-  createSlice,
-} from "@reduxjs/toolkit";
-import { fetchPopularMovies } from "./api"; */
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { MOVIEDB_API_URL } from "src/common/constants";
 
@@ -37,50 +30,3 @@ export const moviesApi = createApi({
 });
 
 export const { useGetMoviesQuery, useGetMovieByIdQuery } = moviesApi;
-
-/* const initialState = {
-  movies: [],
-  status: "idle", // 'idle' / 'loading' / 'succeeded' / 'failed'
-  error: null,
-}; */
-
-/* export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
-  const data = await fetchPopularMovies();
-  return data.data;
-}); */
-
-/* const moviesSlice = createSlice({
-  name: "movies",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder
-      .addCase(fetchMovies.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(fetchMovies.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.movies = action.payload.results;
-      })
-      .addCase(fetchMovies.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      });
-  },
-}); */
-
-/* export const selectAllMovies = createSelector(
-  [(state) => state.movies.movies],
-  (mov) => {
-    const movies = [...mov];
-    movies.sort((movieA, movieB) => movieB.vote_average - movieA.vote_average);
-    return movies;
-  }
-); */
-
-/* export const selectMoviesStatus = (state) => state.movies.status;
-export const selectMoviesError = (state) => state.movies.error;
-export const selectMovieById = (state, movieId) =>
-  state.movies.movies.find((movie) => movie.id === Number(movieId));
-
-export default moviesSlice.reducer; */
