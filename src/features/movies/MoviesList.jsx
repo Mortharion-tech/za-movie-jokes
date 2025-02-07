30af981eb37bcda6c908a246dc2b6d80f7d58ecb
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-07 05:48:04 +0100

    Code Cleanup

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,50 +1,9 @@
-/* import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"; */
-import {
-  Flex,
-  Heading,
-  SimpleGrid,
-  Spinner /* , useToast  */,
-} from "@chakra-ui/react";
+import { Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
 import MovieCard from "./MovieCard";
-import {
-  /* fetchMovies,
-  selectAllMovies,
-  selectMoviesError,
-  selectMoviesStatus, */
-  useGetMoviesQuery,
-} from "./moviesApi";
+import { useGetMoviesQuery } from "./moviesApi";
 
 function MoviesList() {
-  /* const dispatch = useDispatch();
-  const toast = useToast();
-  const movies = useSelector(selectAllMovies);
-  const moviesStatus = useSelector(selectMoviesStatus);
-  const moviesError = useSelector(selectMoviesError); */
-
-  /*   console.log(movies);
-  console.log(moviesStatus); */
-
-  /* useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await dispatch(fetchMovies()).unwrap();
-      } catch (err) {
-        toast({
-          title: "Failed to load movies",
-          description:
-            "Please refresh the page and check your internet connection!",
-          status: "error",
-          duration: 9000,
-          isClosable: true,
-        });
-      }
-    };
-
-    fetchData();
-  }, [dispatch, toast]); */
-
   const { data, isError, error, isLoading, isSuccess } = useGetMoviesQuery();
 
   let content;
